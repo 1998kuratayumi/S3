fef30b32b56b839bfd0a4ceeb1eeff7c00596f59
1998kuratayumi <79153040+1998kuratayumi@users.noreply.github.com>
2022-09-06 16:31:59 +0900

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,17 @@ Rails.application.configure do
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'kurata.yumi.infratop@gmail.com',
+    password:             'nydmisxgarapixei',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
@@ -17,5 +18,12 @@ Rails.application.routes.draw do
     get "search", to: "users#search"
   end
   resources :chats, only: [:create, :show]
+
+  resources :groups do    
+    get "join" => "groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
