d17b45ffef64214e0da2a03f0a81e3874bdf2356
1998kuratayumi <79153040+1998kuratayumi@users.noreply.github.com>
2022-09-08 10:35:15 +0900

    mail fail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,18 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            '',
+    password:             '',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -69,22 +81,11 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.hosts << "a8c9cb2b77b24968bf1f0967084f4a3f.vfs.cloud9.us-east-1.amazonaws.com"
   config.hosts << "49e69874be6d4ef3a0718e9b15430b98.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'gmail.com',
-    user_name:            'kurata.yumi.infratop@gmail.com',
-    password:             'nydmisxgarapixei',
-    authentication:       'login',
-    enable_starttls_auto: true
-  }
+
 end
